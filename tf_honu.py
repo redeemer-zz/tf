40d4b3bef43b0f173e70ceade3f80140123c1fc1
Cyril Oswald <cyril.oswald@gmail.com>
2017-11-01 17:10:15 +0100

    honu sin, MG

--- tf_honu.py
+++ tf_honu.py
@@ -1,32 +1,51 @@
 import numpy as np
+from copy import copy
 
 def add_bias(X):
-    bias_vec = np.ones([X.shape[0],1])
-    return np.hstack((bias_vec, X))
+    if len(X.shape) == 1:
+        return np.concatenate((np.array([1]), X))
+    else:
+        bias_vec = np.ones([X.shape[0],1])
+        return np.hstack((bias_vec, X))
 
-def make_qnu_line(line):
-    nXh = np.sum(range(len(line)+1)) 
-    Xh = np.zeros(nXh)
+def honu_xcol_iteration(input_vec, output, order, start = 0, submul = []):
+    if type(input_vec) == list:
+        input_vec = np.array(input_vec)
+    order_it = order - 1
+    for idx, val in enumerate(input_vec[start:], start):
+        submul_it = copy(submul)
+        submul_it.append(val)
+        if order_it > 0:
+            honu_xcol_iteration(input_vec, output, order_it, idx, submul_it)
+        else:
+            output.append(np.prod(submul_it))
 
-    idxh = 0
-    for idx, xi in enumerate(line):
-        for xj in line[idx:]:
-            Xh[idxh] = xi*xj
-            idxh += 1 
+def make_honu_row(input_row, order, bias = True):
+    ivb = add_bias(input_row)
+    honu = []
+    honu_xcol_iteration(ivb, honu, order)
+    if bias:
+        return honu
+    else:
+        return honu[1:]
 
-    return Xh
+def make_honu_batch(input_matrix, order, bias = True):
+    if type(input_matrix) in (list, tuple):
+        input_matrix = np.array(input_matrix)
 
-def make_lnu(X):
-    return add_bias(X)
+    honu_elements = 0
+    n = input_matrix.shape[1]
+    for j in range(order):
+        honu_elements += int(np.math.factorial(n+j)/(np.math.factorial(j+1)*(np.math.factorial(n-1))))
 
-def make_qnu(X, bias = False):
-    nXh = np.sum(range(X.shape[1]+1)) 
-    Xh = np.zeros([X.shape[0], nXh])
+    if bias: honu_elements += 1
 
-    for row_idx in range(X.shape[0]):
-        Xh[row_idx,:] = make_qnu_line(X[row_idx,:])
-    if bias: Xh = add_bias(Xh)
-    return Xh
+    output = np.zeros((input_matrix.shape[0], honu_elements))
+    
+    for idx, row in enumerate(input_matrix):
+        output[idx, :] = make_honu_row(row, order, bias)
+    
+    return output
 
 def honu_net(HONUs = []):
     net = HONUs[0]
--- tfhonu2.py
+++ tfhonu2.py
@@ -6,22 +6,44 @@ from tf_honu import *
 import matplotlib
 matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
+import copy
+import sympy as sp
 
 max_loss = 1e-10
 
-time = np.arange(0,100,0.1, dtype=np.float32)
-data = 2 * time 
+
+
+
+
+time = np.arange(0,30,0.1, dtype=np.float32)
+data = np.sin(time)
 y = data #+ np.random.uniform(-0.05, 0.05, len(time))
 # y = data
 # y = np.arange(10)
-wlen = 2
-pred = 1
+
+N = 300
+a = 0.2
+b = 0.8
+c = 0.9
+d = 20
+h = 10.0
+y = np.zeros(N)
+y[0] = 0.1
+for k in range(0,N-1):
+    y[k+1]  = (c*y[k]) + ( (a*y[k-d]) / (b + ( y[k-d]**h)) )    
+
+wlen = 11
+pred = 3
 
 y, _ = normalize(y)
 
-x_train = swv(y, wlen)[:100,:]
-y_train = swo(y, wlen, 1)[:100,:]
+train_n = 600
+
+x_train = swv(y, wlen)[:train_n,:]
+y_train = swo(y, wlen, 1)[:train_n,:]
 x_train, _ = ioa(x_train, y_train)
+
+x_train = make_honu_batch(x_train, 1, True)
 
 # print(x_train)
 # print(y_train)
@@ -48,26 +70,40 @@ y = tf.placeholder(tf.float32, name='y')
 
 # W = tf.get_variable("W", shape=[x_train.shape[1]],
 #                     initializer=xavier_initializer())
-W = tf.Variable(xavier_init(x_train.shape[1]))
+
+W = tf.Variable(np.random.uniform(-1,1,x_train.shape[1]).astype(np.float32)) #tf.Variable(xavier_init(x_train.shape[1]))
+print(W)
 honu = tf.reshape(tf.matmul(X,tf.expand_dims(W,1)), [-1])
 loss = tf.reduce_sum(tf.square(honu - y)) # sum of the squares
 # train = tf.train.AdamOptimizer(0.5).minimize(loss)
-train = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
+train = tf.train.GradientDescentOptimizer(0.0001).minimize(loss)
 print('Training...')
+
+error_mean = None
+error_mean_stop = 10e-2
 with tf.Session() as sess:
     
     init = tf.global_variables_initializer().run()
     
-    for i in range(5000):
-        sess.run(train, {X: x_train, y: y_train})
-        error = sess.run(loss, {X: x_train, y: y_train})
-        weight = sess.run(W)
+    try:
+        for i in range(1):
+            sess.run(train, {X: x_train, y: y_train})
+            error = sess.run(loss, {X: x_train, y: y_train})
+            weight = sess.run(W)
 
-        if error < max_loss: break
-        print('Iteration {0}, error: {1}'.format(i, error))
+            if error_mean == None:
+                error_mean = error
+            else:
+                error_mean = (29*error_mean + error) / 30
+                if np.abs(error - error_mean) < error_mean_stop: break
+            if error < max_loss: break
+            print('Iteration {0}, error: {1}'.format(i, error))
+    
+    except KeyboardInterrupt:
+        pass
 
     print('Iteration {0}, error: {1}'.format(i, error))
     # print(weight)
-    plt.plot(time[:100], y_train)
-    plt.plot(time[1:101], sess.run(honu, {X: x_train, y: y_train}), 'r')
+    plt.plot(y_train)
+    plt.plot(sess.run(honu, {X: x_train, y: y_train}), 'r')
     plt.show()
